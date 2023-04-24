b8f75c897531bb825558428f65da555b3c6e75aa
yqian <44333862+yqian108@users.noreply.github.com>
2023-04-24 19:11:26 +0800

    Add files via upload

---
+++ func/generate_PK.m
@@ -0,0 +1,14 @@
+function PK = generate_PK(P1,K)
+%generate_PK: generate probability matrix using K times Kronecker power
+%   INPUT: P1 Kronecker initiator, K
+%   OUTPUT: Kronecker probility matrix of size m^K by m^K
+    tmp_K = K;
+    tmp_P = P1;
+    while tmp_K > 1
+        tmp_P = kron(P1,tmp_P);
+        %tmp_P = kron(tmp_P,P1);
+        tmp_K = tmp_K - 1;
+    end
+    PK = tmp_P;
+end
+
---
+++ func/generate_Theta.m
@@ -0,0 +1,24 @@
+function [Theta] = generate_Theta(k,m,p)
+% align: Theta*vec(X)
+% X = [a,c;b,d]
+% Theta = [times(a), times(c), times(b), times(d)]
+
+N = m^k;         
+Theta = zeros(N^2,m^2);
+
+for i = 1:m
+    for j = 1:m
+        Z = zeros(m);
+        Z(i,j) = 1;
+        temp = Z;
+        for q = 2:k
+            temp = kron(temp, ones(m)) + kron(ones(m^(q-1)), Z);
+        end
+        Theta(:,m*(j-1)+i) = temp(:);
+    end
+end
+
+Theta = Theta/N;
+Theta = p^(k-1)*Theta;
+end
+
