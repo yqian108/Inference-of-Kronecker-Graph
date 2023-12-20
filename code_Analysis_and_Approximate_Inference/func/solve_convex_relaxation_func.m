c5f588c8be8c033db8e17b0bc90e44cc860ee535
yqian108 <773620343@qq.com>
2023-12-20 22:20:23 +0800

    commit

--- code_Analysis_and_Approximate_Inference/func/solve_convex_relaxation_func.m
+++ code_Analysis_and_Approximate_Inference/func/solve_convex_relaxation_func.m
@@ -1,14 +1,14 @@
 function [hat_x] = solve_convex_relaxation_func(y_block, Theta_block,N,x_init, lambda, max_iter, tolerance) 
-    % Find the solution of min 1/(N^2) * || vec(S_approx_shrink) - Theta*x - N*e||_2^2 + lambda ||e||_1
+    % Find the solution of min ||vec(S_approx_shrink) - Theta*x - sqrt(N)*e||_2^2 + lambda ||e||_1
     x_pre = x_init;
     para_lambda  = lambda/sqrt(N);
     obj_pre = 0;
     for i = 1:max_iter
-        % step 1: argmin_e 1/(N^2) * || vec(S_approx_shrink) - Theta*x - N*e||_2^2 + lambda ||e||_1
+        % step 1: argmin_e ||vec(S_approx_shrink) - Theta*x - sqrt(N)*e||_2^2 + lambda ||e||_1
         y = (y_block - Theta_block*x_pre)/sqrt(N);
         e = sign(y).*max(abs(y)-para_lambda /2,0);
       
-        % step 2: argmin_x 1/(N^2) * || vec(S_approx_shrink) - Theta*x - N*e||_2^2 
+        % step 2: argmin_x ||vec(S_approx_shrink) - Theta*x - sqrt(N)*e||_2^2 
         y = y_block(:) - sqrt(N)*e;
         x = (Theta_block'*Theta_block)^(-1)*Theta_block'*y;
        
